9efb9d8f48014e2e895b2aed389dff9c8da218e9
Jacob E. Overgaard <s144029@student.dtu.dk>
2018-03-11 13:12:42 +0100

    Made a working example for a simple lowpass RC-filter

--- demos/demo.py
+++ demos/demo.py
@@ -13,19 +13,30 @@ def tf(s):
 	return 1/(1 + R * C * s)
 
 if __name__ == '__main__':
-	# Fetch data from "lowpassRC.txt"
+	# Fetch data from exported .txt file
 	fname = "lowpassRC.txt"
 	freq, mag, deg = ltspice.ltspiceReadAC(fname)
 
 	llimit = np.log10(1) # 1 Hz from LTspice
 	hlimit = np.log10(100*10**3) # 100 kHz from LTspice
 	f = np.logspace(llimit , hlimit , num=1000)
-	H = tf(1j*np.pi*f)
+	H = tf(1j * 2 * np.pi * f)
 
-	plt.figure()
-	plt.semilogx(f, 20*np.log10(np.abs(H)), color = 'black', linewidth='2')
+	# Magnitude
+	plt.figure(1)
+	plt.semilogx(f, 20*np.log10(np.abs(H)), color = 'black', linewidth=2, label='Calculated')
+	plt.plot(freq, mag, color = 'red', linestyle=':', linewidth=2, label='Simulated')
 	plt.ylabel('Gain / dB')
 	plt.xlabel('Frequency / Hz')
+	plt.legend()
 	plt.grid()
+	plt.show()
 
+	# Phase
+	plt.figure(2)
+	plt.semilogx(f, np.angle(H)*180/np.pi, color = 'black', linewidth=2, label='Calculated')
+	plt.plot(freq, deg, color = 'red', linestyle=':', linewidth=2, label='Simulated')
+	plt.ylabel('Phase / $\degree$')
+	plt.xlabel('Frequency / Hz')
+	plt.grid()
 	plt.show()
